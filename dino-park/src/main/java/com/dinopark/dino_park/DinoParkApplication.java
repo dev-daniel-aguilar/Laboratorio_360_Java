56b70d073f2912ed387ef8ef1dbce1f071ef3c68
Daniel <dan470074@gmail.com>
2026-05-20 19:30:24 -0600

    feat: Se deja la estructura principal del proyecto

---
+++ dino-park/src/main/java/com/dinopark/dino_park/DinoParkApplication.java
@@ -0,0 +1,13 @@
+package com.dinopark.dino_park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinoParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinoParkApplication.class, args);
+	}
+
+}
---
+++ dino-park/src/test/java/com/dinopark/dino_park/DinoParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinopark.dino_park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinoParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
